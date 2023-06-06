aeb50cea73032aad0b869843710161ec0d896805
T <zahiruddin_t@dell.com>
2023-06-06 09:45:05 +0530

    added KeyValueStore class

---
+++ src/KVLite.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using KVLite.Core.Storage;
+
+var kvStore = new KeyValueStore();
+
+// Test the KeyValueStore methods
+kvStore.Set("testKey", "testValue");
+
+var value = kvStore.Get("testKey");
+Console.WriteLine(value);
+Console.ReadLine();
+                
---
+++ src/KVLite.Core/Storage/KeyValueStore.cs
@@ -0,0 +1,32 @@
+﻿namespace KVLite.Core.Storage
+{
+    public class KeyValueStore
+    {
+        private readonly Dictionary<string, string> keyValuePairs;
+
+        public KeyValueStore()
+        {
+            keyValuePairs = new Dictionary<string, string>();
+        }
+
+
+        public void Set(string key, string value)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+
+            keyValuePairs[key] = value;
+        }
+
+
+        public string? Get(string key)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+
+            return keyValuePairs.TryGetValue(key, out var value) ? value : null;
+        }
+
+    }
+}
+    
